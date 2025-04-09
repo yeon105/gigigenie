0e9db73710024c8b25e0c6e02dc5783fb514d4b4
Heeyeon An <gmldus4613@naver.com>
2025-04-09 18:04:32 +0900

    제품 전체조회 기능 추가

---
+++ backend/src/main/java/com/gigigenie/domain/product/dto/ProductResponse.java
@@ -0,0 +1,14 @@
+package com.gigigenie.domain.product.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+public class ProductResponse {
+    private Integer id;
+    private String name;
+    private String icon;
+}
