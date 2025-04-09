f6d064c7cc713941b12b8434bcf545e8eef2189c
Heeyeon An <gmldus4613@naver.com>
2025-04-09 10:03:58 +0900

    pdf 업로드 수정

--- backend/src/main/java/com/gigigenie/domain/chat/repository/EmbeddingRepository.java
+++ backend/src/main/java/com/gigigenie/domain/chat/repository/EmbeddingRepository.java
@@ -3,4 +3,5 @@ package com.gigigenie.domain.chat.repository;
 import com.gigigenie.domain.chat.entity.Embedding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmbeddingRepository extends JpaRepository<Embedding, Long> {}
+public interface EmbeddingRepository extends JpaRepository<Embedding, Integer> {
+}
