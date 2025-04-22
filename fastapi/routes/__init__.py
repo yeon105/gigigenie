e825150af879174120d1a612e63eda889be31e46
Heeyeon An <gmldus4613@naver.com>
2025-04-22 13:28:03 +0900

    fastapi 삭제

--- fastapi/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .upload import router as upload_router
-from .search import router as search_router
-# from .collections import router as collections_router
--- fastapi/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .embedding import get_embeddings
-from .storage import search_documents
-from .llm import create_answer_with_gemini
-from .query import search_documents_with_answer
