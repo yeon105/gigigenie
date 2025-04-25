19f06a43804e5553310f9d665f03ba63d0cd0c7d
Heeyeon An <gmldus4613@naver.com>
2025-04-25 17:03:49 +0900

    쿠키 설정 변경

--- backend/src/main/java/com/gigigenie/util/CookieUtil.java
+++ backend/src/main/java/com/gigigenie/util/CookieUtil.java
@@ -13,8 +13,8 @@ public class CookieUtil {
         ResponseCookie cookie = ResponseCookie.from(name, value)
                 .path("/") // CORS 설정, 모든 경로에서 접근 가능, localhost:8080/api에서 path: "/api"
                 .httpOnly(true) // XSS 방지, JS에서 쿠키값을 읽는 것을 불가, XSS란?
-                .secure(true)   // HTTPS, SSL 설정
-                .sameSite("None")  // CORS 설정, 모든 도메인에서 접근 가능, None: 모든 도메인에서 접근 가능, Lax: 일부 도메인에서 접근 가능, Strict: 도메인에서만 접근 가능
+                .secure(false)   // HTTPS, SSL 설정
+                .sameSite("Lax")  // CORS 설정, 모든 도메인에서 접근 가능, None: 모든 도메인에서 접근 가능, Lax: 일부 도메인에서 접근 가능, Strict: 도메인에서만 접근 가능
                 .maxAge(mins * 60) // maxAge 설정 (초)
                 .build();
 
@@ -25,8 +25,8 @@ public class CookieUtil {
         ResponseCookie cookie = ResponseCookie.from(token, "")
                 .path("/")
                 .httpOnly(true)
-                .secure(true)
-                .sameSite("None")
+                .secure(false)
+                .sameSite("Lax")
                 .maxAge(0L)
                 .build();
 
